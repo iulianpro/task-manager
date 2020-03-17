f05ddd062648801b3b995d5fbbecad8d5a3eb955
Iulian P <iulian.prodea@gmail.com>
2020-03-17 23:47:18 +0000

    Project step 2

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ if path.exists("env.py"):
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
 mongo = PyMongo(app)
 
